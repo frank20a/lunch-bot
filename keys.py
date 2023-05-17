abd2d312b78a4edad5f736b106a02b41040b9816
Frank Fourlas <frank.fourlas@gmail.com>
2023-05-17 13:51:07 +0300

    Vercel

--- keys.py
+++ keys.py
@@ -4,5 +4,8 @@ from glob import glob
 
 keys = {}
 for filename in glob(os.path.join(os.getcwd(), 'keys/*.key')):
-    with open(filename, 'r') as f:
-        keys[os.path.basename(filename).strip('.key')] = f.read()
+    try:
+        with open(filename, 'r') as f:
+            keys[os.path.basename(filename).strip('.key')] = f.read()
+    except FileNotFoundError:
+        keys[os.path.basename(filename).strip('.key')] = os.environ.get(f'{os.path.basename(filename).strip(".key")}_KEY')
